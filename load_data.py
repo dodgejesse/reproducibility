cda411c76868e603bb1531186f3cace1127f3f2d
Jesse Dodge <dodgejesse@gmail.com>
2019-03-19 13:03:01 -0700

    now uses what should be a correct formula for computing the CDF of the max, then computes the PDF from that.

--- load_data.py
+++ load_data.py
@@ -6,53 +6,78 @@ def from_file(filename = "/home/jessedd/data/reprocudibility/imdb_search.csv"):
     lines = []
     with open(filename, "r") as f:
         lines = f.readlines()
-    process_lines(lines, keys_and_indices)
-    print(len(lines))
+    data = process_lines(lines, keys_and_indices)
+    return data
 
 def process_lines(lines, keys_and_indices):
     possible_values = {}
     data = {}
     for line in lines:
-        # the lines 
-        #if '"' in line:
+        if line.startswith("Task_Name"):
+            continue
         update_possible_values(line, possible_values, keys_and_indices)
         extract_data(line, data, keys_and_indices)
-    for k in possible_values:
-        print(k, possible_values[k])
+
+    if False:
+        print_num_experiments(data)
+        
+        for k in possible_values:
+            print(k, possible_values[k])
+            
+    return data
         
 
 def extract_data(line, data, keys_and_indices):
     split_line = line.split(",")
-    if "data_size" not in data:
-        data["data_size"] = {}
+    cur_data_size = int(split_line[keys_and_indices["data_size"]])
+    cur_classifier = split_line[keys_and_indices["classifier"]]
 
-    cur_data_size = split_line[keys_and_indices["data_size"]]
-    if cur_data_size not in data["data_size"]:
-        data["data_size"][cur_data_size] = {}
 
-    if split_line[keys_and_indices["data_size"]] not in data["data_size"]:
+    if split_line[keys_and_indices["accuracy"]] == '':
+        print(cur_data_size, cur_classifier)
+        return
+
+    else:
+        cur_accuracy = float(split_line[keys_and_indices["accuracy"]])
+
+
+
+
+    if cur_data_size not in data:
+        data[cur_data_size] = {}
+
+    if cur_classifier not in data[cur_data_size]:
+        data[cur_data_size][cur_classifier] = []
+
+    data[cur_data_size][cur_classifier].append(cur_accuracy)
         
 
 def update_possible_values(line, possible_values, keys_and_indices):
     split_line = line.split(",")
     for key_name in keys_and_indices:
+        if key_name == "accuracy":
+            continue
         if key_name not in possible_values:
             possible_values[key_name] = set()
         possible_values[key_name].add(split_line[keys_and_indices[key_name]])
 
-    # note this method of splitting doesn't work for the aggregations.
-    
-    #possible_values["experiment_name"].add(split_line[2])
-    #possible_values["experiment_id"].add(split_line[3])
-    #possible_values["throttle"].add(split_line[-2])
-    
+
+def print_num_experiments(data):
+    counters = {}
+    for data_size in data:
+        for classifier in data[data_size]:
+            counters[data_size + "_" + classifier] = len(data[data_size][classifier])
+
+    for counter in counters:
+        print(counter, counters[counter])
 
 def imdb_keys_and_indices():
     return {
         "experiment_name": 2,
         "experiment_id": 3,
         "classifier":-11,
-        "data_size": -2
+        "data_size": -2,
+        "accuracy": 4,
     }
     
 
--- samplemax.py
+++ samplemax.py
@@ -3,10 +3,46 @@
 
 import load_data
 
-def main():
+def compute_sample_maxes():
     data = load_data.from_file()
 
+    sample_maxes = {}
+    for data_size in data:
+        if data_size not in sample_maxes:
+            sample_maxes[data_size] = {}
+        for classifier in data[data_size]:
+
+            sample_maxes[data_size][classifier] = sample_max(data[data_size][classifier])
+    return sample_maxes
+            
+def sample_max(cur_data):
+
+    cur_data.sort()
+    N = len(cur_data)
+    pdfs = []
+    for n in range(1,N+1):
+        # the CDF of the max
+        F_Y_of_y = []
+        for i in range(1,N+1):
+            F_Y_of_y.append((i/N)**n)
+
+        f_Y_of_y = []
+        cur_cdf_val = 0
+        for i in range(len(F_Y_of_y)):
+            f_Y_of_y.append(F_Y_of_y[i] - cur_cdf_val)
+            cur_cdf_val = F_Y_of_y[i]
+        
+        pdfs.append(f_Y_of_y)
+
+    expected_max_cond_n = []
+    for n in range(N):
+        # for a given n, estimate expected value with \sum(x * p(x)), where p(x) is prob x is max.
+        cur_expected = 0
+        for i in range(N):
+            cur_expected += cur_data[i] * pdfs[n][i]
+        expected_max_cond_n.append(cur_expected)
+    return expected_max_cond_n
 
 if __name__ == '__main__':
-    main()
+    compute_sample_maxes()
     
